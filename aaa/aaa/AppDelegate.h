0ccc8aefc4fb5dc5403f00be4c4e74c27898bc79
Lirual <ios-44@acc80861.ipt.aol.com>
2015-10-30 11:13:01 +0800

    新建

---
+++ aaa/aaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaa
+//
+//  Created by iOS－44 on 15/10/30.
+//  Copyright © 2015年 iOS－44. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aaa/aaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaa
+//
+//  Created by iOS－44 on 15/10/30.
+//  Copyright © 2015年 iOS－44. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
